ff0500e44a92f6298a6079ef7f7abad78610e4ea
[jan <=>
2024-09-20 15:12:25 +0800

    Add Waiting Time Computation. Running and No error.

--- labact.cpp
+++ labact.cpp
@@ -7,34 +7,30 @@ int main()
     int processes;
     cout << "Input how many processes: "; cin >> processes;
 
-    int at[processes], bt[processes];
+    int at[processes], bt[processes], wt[processes];
     cout << "Input ARRIVAL TIME & BURST TIME in each processes: "; 
     cout << "\n==================================="<<endl;
     for (int i=0; i<processes; i++)
     {
-        cout << "Process" << "[" << i << "]" ; //process header
-        cout << "\nArrival Time" << "[" << i << "]" << ": "; cin >> at[i]; // Arrival Time
-        cout << "Burst Time" << "[" << i << "]" << ": "; cin >> bt[i]; // Burst Time
+        cout << "Process" << "[" << i + 1 << "]" ; //process header
+        cout << "\nArrival Time" << "[" << i + 1 << "]" << ": "; cin >> at[i]; // Arrival Time
+        cout << "Burst Time" << "[" << i + 1<< "]" << ": "; cin >> bt[i]; // Burst Time
         cout << "\n==================================="<<endl;
     }
     
 
-    // On Process for Waiting Time
-    int wt[processes];
-    for (int i=1; i<processes; i++)
-    {
-        wt[i] = 0;
-        for (int j=0; j<processes; j++){
-            wt[i] = wt[i] + bt[j];   //Waiting Time = Waiting Time + Burst Time
-        }
-        cout << "\n" << wt[i];
+    // Computing for Waiting Time
+    wt[0] = 0;
+    for (int i=1; i<processes; i++){
+        wt[i] = wt[i - 1] + bt[i - 1];  //Waiting Time = Constant Zero (0) + Burst Time
     }
+    
 
-    // Output
-    cout << "Process\tArrival Time\tBurst Time";
+    // Output Section
+    cout << "Process\tArrival Time\tBurst Time\tWaiting Time";
     for (int i=0; i<processes; i++){
-        cout << "\n" << i << "\t" << at[i] << "\t" << bt[i];
+        cout << "\n" << i << "\t" << at[i] << "\t\t" << bt[i] << "\t\t" << wt[i];
     }
 
-
+    return 0;
 }
